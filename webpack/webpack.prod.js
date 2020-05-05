b2b6c05868de911ae644abdd8f47bb5a4d47e255
curtainTan <1948750473@qq.com>
2020-05-05 16:20:33 +0800

    完善 dll

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -19,6 +19,7 @@ module.exports = merge(common, {
         new DLLReferencePlugin({
             manifest: path.resolve( __dirname, "../dll/react.manifest.json" )
         }),
+        // 引入资源
         new AddAssetHTMLWebpackPlugin({
             filepath: path.resolve( __dirname, "../dll", "react.dll.js" )
         })
@@ -27,8 +28,17 @@ module.exports = merge(common, {
         minimizer: [
             new OptimizeCSSAssetsPlugin({})
         ],
+        // 代码分割
         splitChunks: {
-            chunks: "all"
+            // 对入口文件 和 异步导入 文件都进行处理
+            chunks: "all",
+            minSize: 30 * 1024,
+            // 医用次数达到 1 ，进行代码分割
+            minChunks: 1,
+            // 入口文件请求的 chunks 不超过 3 个
+            maxInitialRequests: 3,
+            // 同时最多加载 5 个请求，异步请求的 chunk 不超过 5 个
+            maxAsyncRequests: 5
         }
     }
 })
